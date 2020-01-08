54840f8fbaba6078eeb1910b5b9149911074def3
SHYAMALA <SHYAMALA@192.168.0.4>
2020-01-08 16:48:57 +0530

    Commit Shragvi project

---
+++ ShragviItConsultancyService/src/main/java/com/shragviItConsultancy/ShragviItConsultancyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shragviItConsultancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShragviItConsultancyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShragviItConsultancyServiceApplication.class, args);
+	}
+
+}
---
+++ ShragviItConsultancyService/src/test/java/com/shragviItConsultancy/ShragviItConsultancyService/ShragviItConsultancyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shragviItConsultancy.ShragviItConsultancyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShragviItConsultancyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
